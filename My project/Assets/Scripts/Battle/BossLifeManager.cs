750583f5d94f96046feae471d8f0db3d9b1f10bd
yuzuu893 <Abe_Yuki>
2026-08-21 17:39:29 +0900

    BossのHP追加

---
+++ My project/Assets/Scripts/Battle/BossLifeManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossLifeManager : MonoBehaviour
+{
+    public Image hpBar;
+
+    [Header("BossÇÃç≈ëÂLifePoint")]
+    public int maxLifePoint = 100;
+
+    void Start()
+    {
+        UpdateLifeUI();
+    }
+
+    void Update()
+    {
+        UpdateLifeUI();
+    }
+
+    void UpdateLifeUI()
+    {
+        if (Life.Instance == null)
+            return;
+
+        hpBar.fillAmount =(float)Life.Instance.lifepoint / maxLifePoint;
+    }
+}
